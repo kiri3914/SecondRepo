4e09b927981f40284afe81a30df8171d59949f16
kiri3914 <t.bektur.2002@gmail.com>
2024-06-25 16:27:56 +0500

    asdlkjhaiuwhdq

---
+++ main.py
@@ -0,0 +1,21 @@
+# int = 3, 5, 7
+# float = 3.45, 5.67, 7.89
+# string = "Hello899", "World", "Python"
+# boolean = True, False, True
+
+# name = "Kirill"
+
+# print(f"Hello {name}. How are you?")
+
+
+# a = int(input("Enter a:"))
+# b = int(input("Enter b:"))
+# print('result')
+# print(a + b)
+
+age = int(input("Enter your age:"))
+
+if age >= 18:
+    print("Ты совершеннолетний")
+else:
+    print("Ты не совершеннолетний")
